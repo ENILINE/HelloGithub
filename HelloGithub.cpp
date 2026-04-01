32e8a05c18b722e0b6736a34411aa7a1c823de30
ENILINE <1323152639@qq.com>
2026-04-01 15:42:40 +0800

    add exe

--- HelloGithub.cpp
+++ HelloGithub.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-  cout << "Hello, Github!" << endl;
-  return 0;
+int main(){
+    int a;
+    cout << "Hello, Github!" << endl;
+    cin >> a;
+    return 0;
 }
